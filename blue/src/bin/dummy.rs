b8eb0fdae6e3f2958ec903bf30739c1eb6a88db7
Matthew Turner <matthew.m.turner@outlook.com>
2021-09-23 13:42:41 -0400

    dummy server

---
+++ blue/src/bin/dummy.rs
@@ -0,0 +1,23 @@
+use std::error::Error;
+use std::io::{BufRead, BufReader};
+use std::net::TcpListener;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:7777")?;
+
+    for stream in listener.incoming() {
+        loop {
+            match &stream {
+                Ok(stream) => {
+                    let mut reader = BufReader::new(stream);
+                    let mut input = String::new();
+                    let byte_count = reader.read_line(&mut input)?;
+                    println!("Read {} bytes", byte_count);
+                    println!("{}", input)
+                }
+                Err(e) => println!("{}", e),
+            }
+        }
+    }
+    Ok(())
+}
