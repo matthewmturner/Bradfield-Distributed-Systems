ce09b2df2995e0adf870fc755420fb97f3f2dcf4
Matthew Turner <matthew.m.turner@outlook.com>
2021-09-23 13:11:46 -0400

    cli opts

--- blue/src/bin/client.rs
+++ blue/src/bin/client.rs
@@ -3,14 +3,20 @@ use std::io;
 use std::net::TcpStream;
 use std::{env, io::Write};
 
+use structopt::StructOpt;
+
 extern crate blue;
 
+use blue::client::args;
 use blue::client::handler::{read_client_request, read_store_response, send_client_request};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-    let port = &args[1];
-    let addr = format!("127.0.0.1:{}", port);
+    // let args: Vec<String> = env::args().collect();
+    let opt = args::Opt::from_args();
+    println!("{:?}", opt);
+    // let port = &args[1];
+    let addr = format!("{}:{}", opt.host, opt.port);
+    println!("{}", addr);
     let mut stream = TcpStream::connect(addr)?;
     let response = read_store_response(&mut stream)?;
     print!("{}", response);
