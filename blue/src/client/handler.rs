05526b009782dcffe2138e6a2e679624070d21cc
Matthew Turner <matthew.m.turner@outlook.com>
2021-09-23 12:33:52 -0400

    Start convert request to pb

--- blue/src/client/handler.rs
+++ blue/src/client/handler.rs
@@ -1,6 +1,11 @@
 use std::io::{self, BufRead, BufReader, Stdin, Write};
 use std::net::TcpStream;
 
+use prost::Message;
+
+use super::super::ipc::message;
+use super::super::ipc::message::request;
+
 pub fn read_client_request(stdin: &mut Stdin) -> io::Result<String> {
     let mut reader = BufReader::new(stdin);
 
@@ -26,4 +31,26 @@ pub fn read_store_response(stream: &mut TcpStream) -> io::Result<String> {
         break bytes;
     };
     Ok(response)
+}
+
+pub fn extract_client_request_type(request: String) -> io::Result<request::Type> {
+    let tokens: Vec<&str> = request.split(" ").collect();
+
+    let request_type: io::Result<message::Request> = match tokens[0] {
+        "get" | "Get" | "GET" => Ok(message::Request {
+            command: message::Get {
+                key: tokens[1].to_string(),
+            },
+        }),
+        "set" | "Set" | "SET" => {
+            let set_tokens: Vec<&str> = tokens[1].split("=").collect();
+            Ok(message::Request {
+                command: message::Set {
+                    key: set_tokens[0],
+                    value: set_tokens[1],
+                },
+            })
+        }
+        _ => Err(()),
+    };
 }
